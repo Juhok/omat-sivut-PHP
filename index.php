eaa4126534c22cb7578daacfa6504944c5478960
Juho Kastemaa <juho.kastemaa@gmail.com>
2025-11-13 17:42:48 +0200

    lisatty uudet tiedostot ja kansiot

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'includes/header.php'; ?>
+<section>
+ <h2>Tervetuloa Juho</h2>
+ <p>Tämä on harjoitustyö — portfolio/etusivu.</p>
+ <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Bengal_tiger_%28Panthera_tigris_tigris%29_female_3_crop.jpg/1024px-Bengal_tiger_%28Panthera_tigris_tigris%29_female_3_crop.jpg" alt="Kuva minusta">
+</section>
+<?php include 'includes/footer.php'; ?>
